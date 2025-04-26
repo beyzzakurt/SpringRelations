de5e6ae279e77b50961d2027f3aad4bd1ae909fb
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-26 14:11:40 +0300

    Add Course entity

---
+++ src/main/java/com/beyzakurt/entities/Course.java
@@ -0,0 +1,22 @@
+package com.beyzakurt.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "course")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
