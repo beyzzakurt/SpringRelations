1482af67703ecceb13ba17b5a656879ab4e321e4
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-26 14:08:46 +0300

    Create DtoStudent object

---
+++ src/main/java/com/beyzakurt/dto/DtoStudent.java
@@ -0,0 +1,20 @@
+package com.beyzakurt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoStudent {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private List<DtoCourse> courses = new ArrayList<>();
+
+}
